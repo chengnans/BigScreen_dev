bf9923929a67ab225b105297014d55bf8ceec5d7
chengnan <chengnans@outlook.com>
2024-12-14 00:17:56 +0800

    移植重构

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="layout">
+    <div class="layout-header">{{ appName }}</div>
+    <div class="layout-main">
+    <Tmap></Tmap>
+    </div>
+  </div>
+</template>
+<script setup>
+import Tmap from "./Tmap.vue";
+
+const appName = '大河岔村基地'
+</script>
+<style lang="scss" scoped>
+.layout {
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+
+  .layout-header {
+    position: absolute;
+    top: 0;
+    z-index: 999;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 64px;
+    padding-bottom: 10px;
+    font-size: 24px;
+    font-weight: bold;
+    color: #fff;
+    background: url('../assets/header_bg.png');
+    background-repeat: no-repeat;
+    background-size: 100% 64px;
+  }
+
+  .layout-main {
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #01355059 1px, transparent 1px),
+    linear-gradient(0deg, #01355059 1px, transparent 1px);
+    background-size: 30px 30px, 30px 30px;
+  }
+}
+</style>
---
+++ src/components/Tmap.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="tdt"></div>
+</template>
+
+<script setup>
+import { onMounted, render, createVNode } from "vue";
+import baseWindow from './baseWindow.vue';
+const init = () => {
+  // 确保 T 是全局对象
+  //const T = window.T;
+
+  // 创建地图实例
+  const map = new T.Map("tdt");
+  map.setMapType(window.TMAP_SATELLITE_MAP);
+  map.centerAndZoom(new T.LngLat(119.990, 36.610), 17);
+
+  // 创建标记点
+  const marker = new T.Marker(new T.LngLat(119.990, 36.610));
+  // map.addOverLay(marker);
+
+// 创建虚拟节点
+  const infoWindowContent = createVNode(baseWindow);
+  let node = document.createElement('div'); // 创建一个div节点
+  render(infoWindowContent, node); // 实例组件挂载到node节点上
+
+  // 监听组件点击事件
+  //   marker.on("click", () => {
+  //   map.openInfoWindow(infoWindow, new T.LngLat(119.999, 36.620));
+  // });
+
+  // 创建天地图信息窗口 infoWindowContent.el：test模板dom
+  let infoWindow = new T.InfoWindow(infoWindowContent.el, {
+    closeButton: false // 关闭天地图弹框默认关闭图标
+  })
+
+  // 打开信息窗口
+  map.openInfoWindow(infoWindow, new T.LngLat(119.990, 36.610));
+
+  // 地图添加标识及信息窗口
+    map.addOverLay(marker,infoWindow)
+
+//   天地图窗口隐藏原有样式及隐藏logo信息放在src/assets/styles/global.scss下
+
+};
+
+
+onMounted(() => {
+  init()
+});
+</script>
+
+<style>
+#tdt {
+  width: 100%;
+  height: 100%;
+}
+</style>
